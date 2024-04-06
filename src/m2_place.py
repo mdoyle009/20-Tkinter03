061d690e58480f5a2bda4a6f2eb48b9bc322eb29
Matthew Doyle <mdoyle009@student.franciscan.edu>
2024-04-06 12:26:59 -0400

    finished m2

--- src/m2_place.py
+++ src/m2_place.py
@@ -5,7 +5,7 @@ import tkinter as tk
 # In this module, all of the _todo_ items will be in one comment because you
 # will be modifying the same block of code as you go.
 #
-# TODO: 1. (1 pt)
+# DONE: 1. (1 pt)
 #
 #   First, create a tkinter window called window.
 #
@@ -15,7 +15,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 2. (4 pts)
+# DONE: 2. (4 pts)
 #
 #   Now, create one frame dimensions 200 by 200.
 #
@@ -30,3 +30,14 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 ###############################################################################
+
+window = tk.Tk()
+frame = tk.Frame(width=200, height=200)
+frame.pack()
+labelA = tk.Label(master=frame, text="Label A", bg="red")
+labelA.place(x=0, y=0)
+labelB = tk.Label(master=frame, text="Label B", bg="blue")
+labelB.place(x=25, y=25)
+labelC = tk.Label(master=frame, text="Label C", bg="green")
+labelC.place(x=50, y=50)
+window.mainloop()
